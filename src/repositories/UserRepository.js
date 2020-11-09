d11d64c983a2d1b97d35922917855629c76bb7f3
Ruben <rubenfricke@gmail.com>
2020-11-09 16:33:14 +0100

    Improved typing syntax and layout

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -25,56 +25,62 @@ const userArray = [
     },
 ];
 
+/*
+* This method communicates with the database and will
+* retrieve all the users in the database.
+* @author Ruben Fricke
+*/
+function returnAllUser() {
+    console.info("Retrieving all users");
+    return userArray;
+}
+
+/*
+* This method communicates with the database and will
+* retrieve the user with the specified id in the database.
+* @author Ruben Fricke
+*/
+function returnUserById(id) {
+    console.info(`Retrieving user with specified id: ${id}`);
+    return userArray.filter(user => user.id == id);
+}
+
+/*
+* This method communicates with the database and will
+* create the specified user into the database.
+* @author Ruben Fricke
+*/
+function createUser(user) {
+    console.info("Creating user");
+    userArray.push(user);
+}
+
+/*
+* This method communicates with the database and will
+* update the user with the specified id.
+* @author Ruben Fricke
+*/
+function updateUser(id, user) {
+    console.info(`Updating user with specified id: ${id}`);
+    let index = userArray.findIndex(user => user.id == id);
+    userArray[index] = user;
+}
+
+/*
+* This method communicates with the database and will
+* delete the user with the specified id.
+* @author Ruben Fricke
+*/
+function deleteUser(id) {
+    console.info(`Deleting user with specified id: ${id}`);
+    let index = userArray.findIndex(user => user.id == id);
+    userArray.splice(index, 1);
+}
+
 module.exports = {
-    /*
-    * This method communicates with the database and will
-    * retrieve all the users in the database.
-    * @author Ruben Fricke
-    */
-    returnAllUser() {
-        console.info("Retrieving all users");
-        return userArray;
-    },
-
-    /*
-    * This method communicates with the database and will
-    * retrieve the user with the specified id in the database.
-    * @author Ruben Fricke
-    */
-    returnUserById(id) {
-        console.info(`Retrieving user with specified id: ${id}`);
-        return userArray.filter(user => user.id == id);
-    },
-
-    /*
-    * This method communicates with the database and will
-    * create the specified user into the database.
-    * @author Ruben Fricke
-    */
-    createUser(user) {
-        console.info("Creating user");
-        userArray.push(user);
-    },
-
-    /*
-    * This method communicates with the database and will
-    * update the user with the specified id.
-    * @author Ruben Fricke
-    */
-    updateUser(id, user) {
-        console.info(`Updating user with specified id: ${id}`);
-        let index = userArray.findIndex(user => user.id == id);
-        userArray[index] = user;
-    },
-
-    /*
-    * This method communicates with the database and will
-    * delete the user with the specified id.
-    * @author Ruben Fricke
-    */
-    deleteUser(id) {
-        console.info(`Deleting user with specified id: ${id}`);
-        let index = userArray.findIndex(user => user.id == id);
-        userArray.splice(index, 1);
-    }
+    returnAllUser,
+    returnUserById,
+    createUser,
+    updateUser,
+    deleteUser
 }
