71715563884ed6511f3ff3d248642bf471e256e2
Ruben <rubenfricke@gmail.com>
2020-11-09 13:48:38 +0100

    Updated the user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,26 @@
-
 const express = require('express');
-
 const router = express.Router();
 
 // Controller Methods
-const { getAllUsers, getUserById } = require('../controllers/userController');
+const { getAllUsers, getUserById, createUser, deleteUser, updateUser } = require('../controllers/userController');
 
+/*
+* Routes for user endpoints without any route parameter
+* @author Ruben Fricke
+*/
 router.route('/')
-  .get(getAllUsers);
+  .get(getAllUsers)
+  .post(createUser);
 
+/*
+* Routes for user endpoints with id route parameter to
+* for specifying a specific user.
+* @author Ruben Fricke
+* @param {id} id is the paramaeter for specifying a specific user.
+*/
 router.route('/:id')
-  .get(getUserById);
+  .get(getUserById)
+  .delete(deleteUser)
+  .put(updateUser);
 
 module.exports = router;
