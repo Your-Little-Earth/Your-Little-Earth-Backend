8abc6f3f354b2d792a4a4e6bd8ab51fb3f318d9e
Ruben <rubenfricke@gmail.com>
2020-11-09 17:04:11 +0100

    Created get all user tests whenever there are users

--- src/tests/userController.test.js
+++ src/tests/userController.test.js
@@ -4,13 +4,28 @@ const { mockRequest, mockResponse } = require('../utils/interceptor');
 const controller = require('../controllers/userController');
 const { getAllUsers } = require('../services/UserService');
 
+const userArray = [
+    {
+        id: 1,
+        username: 'Henry',
+        email: 'h.enry@mail.com',
+        password: '179909b745f81f03f177a3079e0ce5e3'
+    },
+    {
+        id: 2,
+        username: 'Stan',
+        email: 'stan@mail.com',
+        password: '3ffa4a71a5aa791a8bc3409f5b15b936'
+    }
+];
+
 describe("Check the GetAllUsers method in the UserController", () => {
 
     /*
-    * Test the GetAllUsers method whenever there are no users
+    * Test the GetAllUsers method whenever there are no users.
     * @author Ruben Fricke
     */
-    test('Getting all user whenever there are no users', () => {
+    test('Getting all users whenever there are no users', () => {
         let req = mockRequest();
         let res = mockResponse();
 
@@ -19,10 +34,33 @@ describe("Check the GetAllUsers method in the UserController", () => {
         controller.getAllUsers(req, res);
 
         expect(res.status).toHaveBeenCalledWith(200);
+        expect(getAllUsers).toHaveBeenCalled();
         expect(res.json).toHaveBeenCalledWith({
             "count": 0,
             "data": [],
-            "success": true});
+            "success": true
+        });
+    });
+
+    /*
+    * Test the GetAllUsers method whenever there are
+    * users stored in the database.
+    * @author Ruben Fricke
+    */
+    test('Getting all users whenever there are users', () => {
+        let req = mockRequest();
+        let res = mockResponse();
+
+        getAllUsers.mockReturnValueOnce(userArray);
+
+        controller.getAllUsers(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
         expect(getAllUsers).toHaveBeenCalled();
+        expect(res.json).toHaveBeenCalledWith({
+            "count": 2,
+            "data": userArray,
+            "success": true
+        });
     });
 });
