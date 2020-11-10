fbbd21d90a8a645849fd951ae4a992a0ce12a122
Ruben <rubenfricke@gmail.com>
2020-11-10 14:55:54 +0100

    Updated event controller for modelstate checking

--- src/controllers/eventControntroller.js
+++ src/controllers/eventControntroller.js
@@ -1,23 +1,29 @@
 const eventService = require('../services/EventService');
 
-const eventArray = [
-    {
-        id: 1,
-        name: 'Event 1',
-        description: 'Description',
-        date: new Date(2020, 1, 1)
-    },
-    {
-        id: 2,
-        name: 'Event 2',
-        description: 'Description',
-        date: new Date(2020, 1, 1)
-    }
-];
-
 exports.eventOverview = (req, res) => {
     const events = eventService.returnAllEvents();
     return res.render('index',{
         events:events
     });
+};
+
+exports.eventCreateView = (req, res) => {
+    return res.sender('create-event');
+};
+
+exports.eventCreate = (req, res) => {
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('description', 'Description is required').notEmpty();
+    req.checkBody('points', 'Points should be specified').notEmpty();
+
+    let errors = req.validationErrors();
+
+    if(errors) {
+        res.render('create-event', {
+            errors: errors
+        });
+    } else {
+        let eventToCreate = req.body;
+        eventService.createEvent(eventToCreate);
+    }
 };
