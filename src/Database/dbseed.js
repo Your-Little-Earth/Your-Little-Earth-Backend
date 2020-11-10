ee2ee6c7d8366d5825c8b6f64a9115401ccc5085
larsverp2803@gmail.com <larsverp2803@gmail.com>
2020-11-10 08:16:04 +0100

    Connected the DB

---
+++ src/Database/dbseed.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.end();
+});
