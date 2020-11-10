3d8459fa3f88f9e4b8dc625fcb59d1c13b04b2de
Ruben <rubenfricke@gmail.com>
2020-11-10 11:53:34 +0100

    Create first basic view with layout

--- src/server.js
+++ src/server.js
@@ -7,10 +7,14 @@ const cors = require('cors');
 const notFound = require('./middleware/notFound');
 const errorHandler = require('./middleware/errorHandler');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 dotenv.config({ path: 'src/config/config.env' });
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // Development Setup
 if (process.env.NODE_ENV === 'development') {
@@ -28,6 +32,10 @@ app.use(express.json());
 
 // All routes here
 app.use('/api/users', require('./routes/user'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 // Custom middleware here
 app.use(notFound);
