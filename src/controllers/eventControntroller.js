14652c5998f6bcc248afb0624ce30f40310e62c7
Ruben <rubenfricke@gmail.com>
2020-11-10 14:16:29 +0100

    Create first implementation for event controller

---
+++ src/controllers/eventControntroller.js
@@ -0,0 +1,24 @@
+const eventService = require('../services/EventService');
+
+const eventArray = [
+    {
+        id: 1,
+        name: 'Event 1',
+        description: 'Description',
+        date: new Date(2020, 1, 1)
+    },
+    {
+        id: 2,
+        name: 'Event 2',
+        description: 'Description',
+        date: new Date(2020, 1, 1)
+    }
+];
+
+exports.eventOverview = (req, res) => {
+    const events = eventService.returnAllEvents();
+    console.log(events);
+    return res.render('index',{
+        events:events
+    });
+};
