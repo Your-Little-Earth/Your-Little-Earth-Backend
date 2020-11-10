654ff4ff579f91ec9c3d24e264ca155cdf007dd8
Ruben <rubenfricke@gmail.com>
2020-11-10 12:57:11 +0100

    Added basic styling

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, './public')));
 
 // Development Setup
 if (process.env.NODE_ENV === 'development') {
