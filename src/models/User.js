364864ac2a9888be85ff2408943c52a67136fb95
Ruben <rubenfricke@gmail.com>
2020-11-11 15:38:17 +0100

    Create User model

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    });
+
+    return User;
+};
