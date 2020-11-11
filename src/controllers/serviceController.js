54f96826cff0e65f030ee2c149b8b31a7f1dfcc5
larsverp2803@gmail.com <larsverp2803@gmail.com>
2020-11-11 14:36:33 +0100

    Created service endpoint

---
+++ src/controllers/serviceController.js
@@ -0,0 +1,53 @@
+const serviceService = require('../services/serviceService')
+
+
+exports.getAllServices = (req, res) => {
+  const services = serviceService.getAllServices();
+  return res.status(200).json({
+    success: true,
+    count: services.length,
+    data: services,
+  });
+};
+
+exports.getServiceById = (req, res) => {
+    let userId = Number(req.params.userid);
+    if(userId <= 0) {
+        return res.status(400).json({
+            success: false,
+            error: 'The specified userid is invalid.',
+        });
+    }
+
+    let service = serviceService.getServiceByUserId(userId);
+    if (!isEmpty(service)) {
+        return res.status(200).json({
+            success: true,
+            data: service
+        });
+    }
+
+  return res.status(404).json({
+    success: false,
+    error: 'No service found with the specified user id.',
+  })
+};
+
+exports.createService = (req, res) => {
+    let serviceToCreate = req.body;
+    if (serviceToCreate == null) {
+        return res.status(400).json({
+            success: false,
+            error: 'No service specified to create.'
+        });
+    }
+    let result = serviceService.createService(serviceToCreate);
+    return res.status(201).json({
+        success: true,
+        data: result
+    });
+};
+
+function isEmpty(obj) {
+    return !Object.keys(obj).length;
+}
---
+++ src/routes/service.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// Controller Methods
+const {
+    getAllServices,
+    getServiceById,
+    createService
+    } = require('../controllers/serviceController');
+
+    router.route('/car')
+     .get(getAllServices)
+     .post(createService);
+
+    router.route('/car/:id')
+     .get(getServiceById)
+
+module.exports = router;
