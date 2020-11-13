9f200faf31c27a69b6aa237a50fc3c4a51e7c61b
larsverp2803@gmail.com <larsverp2803@gmail.com>
2020-11-13 12:24:24 +0100

    Oops I sended the users password :poop:

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -8,7 +8,7 @@ exports.loginUser = async (req, res) => {
     console.log(check.status);
     if(check.status) {
         const accesToken = 'Bearer '+ jwt.sign(check.data, process.env.ACCES_TOKEN_SECRET);
-
+        check.data.password = 'This is a secret password my friend'
         return res.status(200).json({
             token: accesToken,
             user: check.data
