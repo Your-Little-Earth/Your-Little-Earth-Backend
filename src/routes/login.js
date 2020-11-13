15fc7224aeb96e19e109ea25deacb708bb39907d
Ruben <rubenfricke@gmail.com>
2020-11-13 09:45:19 +0100

    Removed unused lines

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,13 +1,35 @@
+/**
+ * @swagger
+ * /login:
+ *  post:
+ *      description: This endpoint is used for retrieving all the events stored in the database.
+ *      parameters:
+ *          -   in: body
+ *              name: Login credentials
+ *              description: The credentials to log in.
+ *              schema:
+ *                  type: object
+ *                  required:
+ *                  -   email
+ *                      password
+ *                  properties:
+ *                      email:
+ *                          type: string
+ *                      password:
+ *                          type: string
+ *      responses:
+ *          '200':
+ *              description: Successfully logged in.
+ *          '401':
+ *              description: Unable to access this endpoint without being authorized.
+ */
+
 const express = require('express');
 const router = express.Router();
 
 // Controller Methods
 const { loginUser } = require('../controllers/loginController');
 
-/*
-* Route for login
-* @author Lars van Erp
-*/
 router.route('/')
   .post(loginUser);
 
